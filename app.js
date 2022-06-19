50d8a5c109cdc3e3dd056e6b7e003c4514a00475
Barzi Jaf <barzi4greatness@gmail.com>
2022-06-19 19:49:43 +0200

    some progress

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const board = document.querySelector("#board");
 let secretNumbers = [];
 let clickCount = 0;
 
+let match = [];
 
 if (document.querySelector(".box")) {
   var boxes = document.querySelectorAll(".box");
@@ -11,6 +12,8 @@ if (document.querySelector(".box")) {
 
 
 selectElement.addEventListener("change", () => {
+
+  // creating secret numbers according to the size of the board
   for (i = selectElement.value / 2; i >= 1; i--) {
     secretNumbers.push(parseInt(i));
     if (i == 1) {
@@ -22,6 +25,7 @@ selectElement.addEventListener("change", () => {
   }
   secretNumbers.sort(() => 0.5 - Math.random());
 
+  // setting up the board
   for (x = selectElement.value; x >= 1; x--) {
     const box = document.createElement("div");
     box.classList.add("box");
@@ -31,8 +35,79 @@ selectElement.addEventListener("change", () => {
   }
   const boxes = document.querySelectorAll(".box");
   boxes.forEach(box => {
-    box.addEventListener("click", () => {
-      clickCount++;
+    box.addEventListener("click", (emoji) => {
+      match.push(box.dataset.number);
+      switch (parseInt(box.dataset.number)) {
+        case 1:
+          emoji = "🤥";
+          break;
+        case 2:
+          emoji = "😄";
+          break;
+
+        case 3:
+          emoji = "🙃";
+          break;
+
+        case 4:
+          emoji = "😎";
+          break;
+
+        case 5:
+          emoji = "😳";
+          break;
+
+        case 6:
+          emoji = "😱";
+          break;
+
+        case 7:
+          emoji = "🥵";
+          break;
+
+        case 8:
+          emoji = "😶‍🌫️";
+          break;
+
+        case 9:
+          emoji = "🤢";
+          break;
+
+        case 10:
+          emoji = "🤡";
+          break;
+
+        case 11:
+          emoji = "💩";
+          break;
+
+        case 12:
+          emoji = "👻";
+          break;
+      }
+      if (match.length >= 2) {
+        if (match[0] == match[1]) {
+          let matchingBox = document.querySelectorAll(`[data-number='${box.dataset.number}']`);
+
+          matchingBox.forEach(mb => {
+            mb.innerHTML = emoji;
+          })
+
+
+          match = [];
+        }
+        else {
+          boxes.forEach(b => {
+            if (b.innerHTML != "") {
+              b.innerHTML = "";
+            }
+          })
+          match = [];
+          console.log("non match")
+        }
+      }
+      box.innerHTML = emoji;
+
 
     });
   })
